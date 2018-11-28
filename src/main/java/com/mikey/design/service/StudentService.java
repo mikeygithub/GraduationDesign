b06ed75576c986ebaa14087af2c0dbb4e55ece98
mikey <biaogejiushibiao@outlook.com>
2018-11-28 18:13:48 +0800

    完成pojo类及Mapper

---
+++ src/main/java/com/mikey/design/service/StudentService.java
@@ -0,0 +1,12 @@
+package com.mikey.design.service;
+
+/**
+ * @author Mikey
+ * @Title:
+ * @Description:
+ * @Email:1625017540@qq.com
+ * @date 2018/11/28 18:10
+ * @Version 1.0
+ */
+public interface StudentService {
+}
--- src/main/java/com/mikey/design/utils/GBM.java
+++ src/main/java/com/mikey/design/utils/GBM.java
@@ -24,7 +24,7 @@ public class GBM {
     public static void main(String[] args) throws Exception {
         List<String> warnings = new ArrayList<String>();
         boolean overwrite = true;
-        File configFile = new File("classpath:/GBM/nbg.xml");
+        File configFile = new File("E:\\MIKEY\\MobileFile\\Image\\GraduationDesign\\src\\main\\resources\\GBM\\nbg.xml");
         ConfigurationParser cp = new ConfigurationParser(warnings);
         Configuration config = cp.parseConfiguration(configFile);
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
