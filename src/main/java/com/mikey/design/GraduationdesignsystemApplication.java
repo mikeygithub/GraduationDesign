e98345c966ab7c5d1f3eb97fb04527c5c7a0f026
mikey <biaogejiushibiao@outlook.com>
2018-12-04 07:50:45 +0800

    待完善登入切换

--- src/main/java/com/mikey/design/GraduationdesignsystemApplication.java
+++ src/main/java/com/mikey/design/GraduationdesignsystemApplication.java
@@ -1,24 +1,1 @@
-package com.mikey.design;
-
-import com.mikey.design.views.login.Login;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@MapperScan(basePackages={"com.mikey.design.Mapper"})
-@SpringBootApplication
-public class GraduationdesignsystemApplication {
-
-
-    public static void main(String[] args) {
-
-        SpringApplication.run(GraduationdesignsystemApplication.class, args);
-
-        showLoginView();
-    }
-    public static void showLoginView(){
-        System.setProperty("java.awt.headless", "false");
-//        new Login();
-//        login.shows();
-    }
-}
+package com.mikey.design;import com.mikey.design.views.login.Login;import org.mybatis.spring.annotation.MapperScan;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;@MapperScan(basePackages={"com.mikey.design.Mapper"})@SpringBootApplicationpublic class GraduationdesignsystemApplication {    public static void main(String[] args) {        SpringApplication.run(GraduationdesignsystemApplication.class, args);        showLoginView();    }    public static void showLoginView(){        System.setProperty("java.awt.headless", "false");//        new Login();//        login.shows();    }}
