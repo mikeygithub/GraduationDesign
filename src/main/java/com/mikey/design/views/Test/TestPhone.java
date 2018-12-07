c1883260cff1159ee8a3a2933438ac6a3b7ed94b
mikey <biaogejiushibiao@outlook.com>
2018-12-07 14:58:38 +0800

    完成学生端

--- src/main/java/com/mikey/design/views/Test/TestPhone.java
+++ src/main/java/com/mikey/design/views/Test/TestPhone.java
@@ -10,6 +10,7 @@ import java.util.regex.Pattern;
  * @date 2018/12/7 14:29
  * @Version 1.0
  */
+
 public class TestPhone {
     public static final String REGEX_MOBILE ="^[1](([3][0-9])|([4][5,7,9])|([5][^4,6,9])|([6][6])|([7][3,5,6,7,8])|([8][0-9])|([9][8,9]))[0-9]{8}$";
     public static final String REGEX_PASSWORD = "^[a-zA-Z0-9]{6,20}$";
